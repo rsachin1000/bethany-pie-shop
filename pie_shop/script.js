fff3749e19ccac0e799bc91ef6dbe6b78ed1eb7c
Sachin Rana <rana@Sachins-MacBook-Air.local>
2023-09-03 18:14:08 +0530

    pie shop website codew

--- pie_shop/script.js
+++ pie_shop/script.js
@@ -1,16 +1,18 @@
 const inventory = {
-    apple: { price: 12, qty: 0 },
-    cherry: { price: 15, qty: 0 },
-    strawberry: { price: 18, qty: 0 }
+    apple: {price: 12, qty: 0},
+    cherry: {price: 15, qty: 0},
+    strawberry: {price: 18, qty: 0}
 };
 
 function sum() {
     let total = 0;
     const keys = Object.keys(inventory);
     keys.forEach(key => {
-        total += inventory[key].price * inventory[key].qty;
-    });
-    return total;
+        total += inventory[key].price * inventory[key].qty
+    }
+    );
+
+    return total
 }
 
 function calculate(box) {
@@ -33,10 +35,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const inputBoxes = document.querySelectorAll('#calculator input');
 
     inputBoxes.forEach(box => {
-        
-        box.addEventListener('change', () => { 
-            totalContainer.textContent = calculate(box);
-        });
+        // box.addEventListener('change', () => { 
+        //     totalContainer.textContent = calculate(box);
+        // });
         
         box.addEventListener('keyup', () => { 
             totalContainer.textContent = calculate(box);
@@ -45,3 +46,5 @@ window.addEventListener('DOMContentLoaded', () => {
     });
     
 });
+
+
